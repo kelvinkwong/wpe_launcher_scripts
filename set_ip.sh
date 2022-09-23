3d85537ac2ff17d43a5ea236cfd09194900d004d
kelvin wong <kelvinwong@outlook.com.au>
2022-09-23 18:00:48 +1000

    fixed ip address 4

--- set_ip.sh
+++ set_ip.sh
@@ -4,7 +4,9 @@ if [[ -f ~/.env ]]
 then
 	. ~/.env
 else
-	echo "export ENV_IP=10.0.0.2" > ~/.env
+	echo 'export ENV_IP=10.0.0.2' > ~/.env
+	echo 'export ENV_INSPECT_APPS=$ENV_IP:9201' >> ~/.env
+	echo 'export ENV_INSPECT_FOXTEL=$ENV_IP:9222' >> ~/.env
 fi
 
 echo "[$0] enter iq ip address (${ENV_IP}):"
