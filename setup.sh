aff25eaafed5880e2ec5feff10a4057199019562
kelvin wong <kelvinwong@outlook.com.au>
2022-09-23 19:51:52 +1000

    remove foxtel stuff

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ ln -sf /usr/local/libexec/webkit2gtk-4.0/MiniBrowser ~/.local/bin/wpe
 
 rm -rf /tmp/wpe_launcher_scripts
 git clone --depth 1 https://github.com/kelvinkwong/wpe_launcher_scripts /tmp/wpe_launcher_scripts
-rm -f /tmp/wpe_launcher_scripts/{kill_wpe.sh,setup.sh}
+rm -f /tmp/wpe_launcher_scripts/{kill_wpe.sh,setup.sh,wpe_inspect_foxtel.sh}
 cp -f /tmp/wpe_launcher_scripts/*.sh ~/Desktop/
 chmod +x ~/Desktop/*.sh
