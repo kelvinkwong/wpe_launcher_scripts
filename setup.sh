3144a12129f6299b3090005cf9042ab12edd9f89
kelvin wong <kelvinwong@outlook.com.au>
2022-09-23 17:46:50 +1000

    fixed permissions

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ln -s /usr/local/libexec/webkit2gtk-4.0/MiniBrowser /usr/local/bin/MiniBrowser
-ln -s /usr/local/libexec/webkit2gtk-4.0/MiniBrowser /usr/local/bin/wpe
+mkdir ~/.local/bin
+ln -s /usr/local/libexec/webkit2gtk-4.0/MiniBrowser ~/.local/bin/
+ln -s /usr/local/libexec/webkit2gtk-4.0/MiniBrowser ~/.local/bin/wpe
 
+rm -rf /tmp/wpe_launcher_scripts
 git clone --depth 1 https://github.com/kelvinkwong/wpe_launcher_scripts /tmp/wpe_launcher_scripts
 rm -f /tmp/wpe_launcher_scripts/{kill_wpe.sh,setup.sh}
 cp -f /tmp/wpe_launcher_scripts/*.sh ~/Desktop/
